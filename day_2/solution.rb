491b13409196904b6c1bc8e66e834f63167b8e5c
Mike Wilson <mike1234@gmail.com>
2019-12-01 22:32:13 -0800

    Improve solution 1.

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -17,34 +17,29 @@ module Day2
   end
   
   def self.process_intcode(inputs, index)
-    while index < inputs.count
+    loop do
 
       input = inputs[index]
 
       case input
       when 1
-        value_1 = inputs[inputs[index + 1]]
-        value_2 = inputs[inputs[index + 2]]
-
-        result = value_1 + value_2
-
-        inputs[inputs[index + 3]] = result
-
-        index += 4
-
+        operation = :+
       when 2
-        value_1 = inputs[inputs[index + 1]]
-        value_2 = inputs[inputs[index + 2]]
-
-        result = value_1 * value_2
-
-        inputs[inputs[index + 3]] = result
-
-        index += 4
-
+        operation = :*
       when 99
         break
+      else
+        puts "Invalid opcode"
       end
+
+      value_1 = inputs[inputs[index + 1]]
+      value_2 = inputs[inputs[index + 2]]
+
+      result = value_1.send(operation, value_2)
+
+      inputs[inputs[index + 3 ]] = result
+      
+      index += 4
     end
     
     inputs
