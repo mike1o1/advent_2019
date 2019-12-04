f0f7b0642d8fe3fcdf921c650008b48627e1c748
Mike Wilson <mike1234@gmail.com>
2019-12-03 21:38:08 -0800

    Day 3 Solution 2.

--- day_3/solution.rb
+++ day_3/solution.rb
@@ -6,13 +6,25 @@ module Day3
     first_path = paths[0]
     second_path = paths[1]
 
-    intersection = trace_wire(first_path) & trace_wire(second_path)
+    intersections = trace_wire(first_path) & trace_wire(second_path)
 
-    intersection.map { |x, y| x.abs + y.abs }.min
+    intersections.map { |x, y| x.abs + y.abs }.min
   end
 
-  def self.problem_2(_)
-    "Not implemented"
+  def self.problem_2(instructions)
+    first_path, second_path = instructions.split("\n")
+
+    intersections = trace_wire(first_path) & trace_wire(second_path)
+
+    step_counts = intersections.map do |x, y|
+      # count how many steps were taken
+      first_steps = trace_wire_steps(first_path, x, y)
+      second_steps = trace_wire_steps(second_path, x, y)
+
+      first_steps + second_steps
+    end
+
+    step_counts.min
   end
 
   private
@@ -44,5 +56,40 @@ module Day3
       end
 
       visited
+    end
+
+    def self.trace_wire_steps(inputs, point_x, point_y)
+      x, y = 0, 0
+
+      steps = 0
+
+      # Go through the inputs until we meet the first intersection
+      # and count how many steps were taken
+
+      inputs.split(",").each do |input|
+        operation = input[0]
+        count = input[1..].to_i
+
+        count.times do
+          steps += 1
+
+          case operation
+          when "R"
+            x += 1
+          when "L"
+            x -= 1
+          when "U"
+            y += 1
+          when "D"
+            y -= 1
+          end
+
+          if x == point_x && y == point_y
+            return steps
+          end
+        end
+      end
+
+      return 0
     end
 end
--- test/day_3_tests.rb
+++ test/day_3_tests.rb
@@ -20,4 +20,22 @@ class Day3Tests < Minitest::Test
 
     assert_equal(135, Day3.problem_1(instructions))
   end
+  
+  def test_day_3_solution_2_example_1
+    wire_path_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+    wire_path_2 = "U62,R66,U55,R34,D71,R55,D58,R83"
+    
+    instructions = "#{wire_path_1}\n#{wire_path_2}"
+    
+    assert_equal(610, Day3.problem_2(instructions))
+  end
+  
+  def test_day_3_solution_2_example_2
+    wire_path_1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+    wire_path_2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+
+    instructions = "#{wire_path_1}\n#{wire_path_2}"
+    
+    assert_equal(410, Day3.problem_2(instructions))
+  end
 end
