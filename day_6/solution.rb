630c591cef72cc325349b2b149c710a87d958a70
Mike Wilson <mike1234@gmail.com>
2019-12-07 09:42:10 -0800

    Day 6 Solution 2.

--- day_6/solution.rb
+++ day_6/solution.rb
@@ -1,12 +1,6 @@
 module Day6
   def self.problem_1(input)
-    orbits = {}
-
-    # Build a map of all the direct orbits
-    input.split("\n").each do |orbit|
-      first, second = orbit.split(")")
-      orbits[second] = first
-    end
+    orbits = self.build_orbits(input)
 
     orbits_count = 0
 
@@ -20,8 +14,42 @@ module Day6
     orbits_count
   end
 
-  def self.problem_2(_)
-    "Not Implemented"
+  def self.problem_2(input)
+    orbits = self.build_orbits(input)
+
+    you_path = self.walk_path(orbits, "YOU")
+    santa_path = self.walk_path(orbits, "SAN")
+
+    intersect = you_path.find do |obj|
+      santa_path.include?(obj)
+    end
+
+    you_path.index(intersect) + santa_path.index(intersect)
+
   end
+
+  private
+
+    def self.build_orbits(input)
+      orbits = {}
+
+      input.split("\n").each do |orbit|
+        first, second = orbit.split(")")
+        orbits[second] = first
+      end
+
+      orbits
+    end
+
+    def self.walk_path(orbits, origin)
+      path = []
+
+      while orbits[origin]
+        origin = orbits[origin]
+        path << origin
+      end
+
+      path
+    end
 
 end
--- test/day_6_tests.rb
+++ test/day_6_tests.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/focus"
 require_relative("../day_6/solution")
 
 class Day6Tests < Minitest::Test
@@ -18,5 +19,25 @@ class Day6Tests < Minitest::Test
     example_input << "K)L\n"
 
     assert_equal(42, Day6.problem_1(example_input))
+  end
+
+  def test_day_6_solution_2_example
+    example_input = ""
+
+    example_input << "COM)B\n"
+    example_input << "B)C\n"
+    example_input << "C)D\n"
+    example_input << "D)E\n"
+    example_input << "E)F\n"
+    example_input << "B)G\n"
+    example_input << "G)H\n"
+    example_input << "D)I\n"
+    example_input << "E)J\n"
+    example_input << "J)K\n"
+    example_input << "K)L\n"
+    example_input << "K)YOU\n"
+    example_input << "I)SAN\n"
+
+    assert_equal(4, Day6.problem_2(example_input))
   end
 end
